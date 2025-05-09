6879e535626115470e17e6fea681b1515f9e4887
Marcelo Moreto <mmoreto@gmail.com>
2025-05-09 20:46:32 -0300

    ajuste de cores

--- lib/ui/core/themes/colors.dart
+++ lib/ui/core/themes/colors.dart
@@ -5,8 +5,6 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const black1 = Color(0xFF101010);
-  static const white1 = Color(0xFFFFF7FA);
   static const grey1 = Color(0xFFF2F2F2);
   static const grey2 = Color(0xFF4D4D4D);
   static const grey3 = Color(0xFFA4A4A4);
@@ -24,22 +22,22 @@ class AppColors {
   static var lightColorScheme = ColorScheme(
     brightness: Brightness.light,
     primary: AppColors.kPrimary,
-    onPrimary: AppColors.white1,
+    onPrimary: AppColors.kWhite,
     secondary: AppColors.kSecondary,
-    onSecondary: AppColors.white1,
+    onSecondary: AppColors.kWhite,
     surface: Colors.white,
-    onSurface: AppColors.black1,
+    onSurface: AppColors.kBlack,
     error: Colors.white,
     onError: Colors.red,
   );
 
-  static const darkColorScheme = ColorScheme(
+  static var darkColorScheme = ColorScheme(
     brightness: Brightness.dark,
-    primary: AppColors.white1,
-    onPrimary: AppColors.black1,
-    secondary: AppColors.white1,
-    onSecondary: AppColors.black1,
-    surface: AppColors.black1,
+    primary: AppColors.kWhite,
+    onPrimary: AppColors.kBlack,
+    secondary: AppColors.kWhite,
+    onSecondary: AppColors.kBlack,
+    surface: AppColors.kBlack,
     onSurface: Colors.white,
     error: Colors.black,
     onError: AppColors.red1,
